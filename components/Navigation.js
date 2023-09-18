f52de34f298df6aaeb944684e1cd3e04423a199d
katongole <katsisaac50@gmail.com>
2023-09-18 08:36:17 +0200

    added a circle and text-content

--- components/Navigation.js
+++ components/Navigation.js
@@ -8,7 +8,7 @@ const navigationItems = [
 ];
 
 const Navigation = () => (
-  <nav className="bg-gray-300 bg-opacity-10 py-6 right-nav">
+  <nav className="backdrop-blur-sm bg-gray-900/25 absolute z-/20 py-6 right-nav">
     <ul className="flex space-x-4 pr-8 ml-80">
       {navigationItems.map((item, index) => (
         <li key={index}>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
 function HomePage() {
-  return (
-  
-      <div className="home-background">
-      Home Page Content</div>
+  const projects = [
+    {
+      title: 'Project 1',
+      category: 'Category 1',
+      url: '/project1',
+      users: [
+        { name: 'User 1', avatar: 'user1.jpg' },
+        { name: 'User 2', avatar: 'user2.jpg' },
+      ],
+    },
+    // Add more projects as needed
+  ];
 
+  return (
+    <div className="home-background">
+    
+      <section className="text-white">
+      
+      <h2>So, you want to travel to</h2>
+      <h1>Space</h1>
+      <p>
+        Let’s face it; if you want to go to space, you might as well
+        genuinely go to outer space and not hover kind of on the edge of it.
+        Well, sit back, and relax because we’ll give you a truly out of this
+        world experience!
+      </p>
+
+      </section>
+      <div
+  className="w-40 h-40 rounded-full inline-flex items-center justify-center bg-white text-gray-700 text-xl font-bold"
+>
+Explore
+</div>
+    </div>
   );
 }
 

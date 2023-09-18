52aa3d7220e475b198c62a1a9268c625a79a647d
katongole <katsisaac50@gmail.com>
2023-09-18 18:26:48 +0200

    styling the circle

--- pages/index.js
+++ pages/index.js
@@ -14,24 +14,28 @@ function HomePage() {
 
   return (
     <div className="home-background">
-    
-      <section className="text-white">
-      
-      <h2>So, you want to travel to</h2>
-      <h1>Space</h1>
-      <p>
-        Let’s face it; if you want to go to space, you might as well
-        genuinely go to outer space and not hover kind of on the edge of it.
-        Well, sit back, and relax because we’ll give you a truly out of this
-        world experience!
-      </p>
+      <div className="container mx-auto flex mt-60">
+        <section
+          className="text-justify text-white w-[26rem] h-[20rem] flex-shrink-0"
+        >
+          <h2 className="text-2xl uppercase font-mono tracking-[.02rem]">So, you want to travel to</h2>
+          <h1 className="pt-3.5 font-serif text-9xl font-bold uppercase">
+  Space
+</h1>
 
-      </section>
-      <div
-  className="w-40 h-40 rounded-full inline-flex items-center justify-center bg-white text-gray-700 text-xl font-bold"
->
-Explore
-</div>
+          <p className="text-base pt-12 font-mono">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well, sit back, and relax because we’ll give you a truly out of
+            this world experience!
+          </p>
+        </section>
+        <div className="flex items-center justify-center flex-grow ml-96 pl-20 mt-32">
+          <div className="w-60 h-60 rounded-full bg-white text-gray-700 text-xl font-bold font-serif flex items-center justify-center">
+            Explore
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

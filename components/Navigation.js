1615ba75664eae79d186eecdc085060da7258b67
katongole <katsisaac50@gmail.com>
2023-09-18 06:17:15 +0200

    alligning nav and giving it a transparent background

---
+++ components/Navigation.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+const navigationItems = [
+  { text: '00 Home', link: '/' },
+  { text: '01 Destination', link: '/About' },
+  { text: '02 Crew', link: '/About' },
+  { text: '03 Technology', link: '/About' },
+];
+
+const Navigation = () => (
+  <nav className="bg-gray-300 bg-opacity-10 py-6 right-nav">
+    <ul className="flex space-x-4 pr-8 ml-80">
+      {navigationItems.map((item, index) => (
+        <li key={index}>
+          <Link href={item.link}>
+            <div className="text-white font-bold hover:text-yellow-300">
+              {item.text}
+            </div>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
+
+export default Navigation;

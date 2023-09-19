4baff4d3ad97ebead97b432b9419a79116ea6691
katongole <katsisaac50@gmail.com>
2023-09-19 17:02:00 +0200

    modified the navigation to stretch from center to right

--- components/Navigation.js
+++ components/Navigation.js
@@ -12,30 +12,33 @@ const Navigation = () => {
   const router = useRouter();
 
   return (
-    <nav className="backdrop-blur-sm bg-gray-900/25 absolute z-20 py-6 right-nav">
-      <ul className="flex space-x-4 pr-8 ml-80">
-        {navigationItems.map((item, index) => {
-          const isCurrentPage = router.pathname === item.link;
-          const hoverClassName = isCurrentPage ? 'hover:text-yellow-300' : 'hover:text-white';
-          const selectedClassName = isCurrentPage ? 'border-b-2 border-yellow-300' : '';
+    <nav className="backdrop-blur-sm bg-gray-900/25 absolute z-20 py-6 right-0 left-90">
+      <div className="container mx-auto">
+        <ul className="flex space-x-12 px-44 ml-75">
+          {navigationItems.map((item, index) => {
+            const isCurrentPage = router.pathname === item.link;
+            const hoverClassName = isCurrentPage ? 'hover:text-yellow-300' : 'hover:text-white';
+            const selectedClassName = isCurrentPage ? 'border-b-2 border-yellow-300' : '';
 
-          // Split the text into two parts: figure and the rest
-          const parts = item.text.split(' ');
-          const figure = parts[0];
-          const rest = parts.slice(1).join(' ');
+            // Split the text into two parts: figure and the rest
+            const parts = item.text.split(' ');
+            const figure = parts[0];
+            const rest = parts.slice(1).join(' ');
 
-          return (
-          <li key={index}>
-            <Link href={item.link}>
-              <div
-              className={`text-white space-x-3 transition duration-300 ${hoverClassName} ${selectedClassName} ${isCurrentPage ? '' : 'hover:border-b-2'}`}
-              >
-              <span className="font-bold">{figure}</span> <span className='font-mono'>{rest}</span>
-              </div>
-            </Link>
-          </li>
-        )})}
-      </ul>
+            return (
+              <li key={index}>
+                <Link href={item.link}>
+                  <div
+                    className={`text-white space-x-3 transition duration-300 ${hoverClassName} ${selectedClassName} ${isCurrentPage ? '' : 'hover:border-b-2'}`}
+                  >
+                    <span className="font-bold">{figure}</span> <span className='font-mono'>{rest}</span>
+                  </div>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </nav>
   );
 };

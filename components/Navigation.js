cf66a541d8a0ba9823a13126a59bfbd19864ee61
katongole <katsisaac50@gmail.com>
2023-09-19 09:17:38 +0200

    adding a border below on a button on select

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const navigationItems = [
   { text: '00 Home', link: '/' },
@@ -7,20 +8,32 @@ const navigationItems = [
   { text: '03 Technology', link: '/About' },
 ];
 
-const Navigation = () => (
-  <nav className="backdrop-blur-sm bg-gray-900/25 absolute z-/20 py-6 right-nav">
-    <ul className="flex space-x-4 pr-8 ml-80">
-      {navigationItems.map((item, index) => (
-        <li key={index}>
-          <Link href={item.link}>
-            <div className="text-white font-bold hover:text-yellow-300">
-              {item.text}
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const Navigation = () => {
+  const router = useRouter();
+
+  return (
+    <nav className="backdrop-blur-sm bg-gray-900/25 absolute z-20 py-6 right-nav">
+      <ul className="flex space-x-4 pr-8 ml-80">
+        {navigationItems.map((item, index) => {
+          const isCurrentPage = router.pathname === item.link;
+          const hoverClassName = isCurrentPage ? 'hover:text-yellow-300 hover:border-b-2' : 'hover:text-white';
+
+          return (
+          <li key={index}>
+            <Link href={item.link}>
+              <div
+                className={`text-white font-bold hover:text-yellow-300 ${
+                  router.pathname === item.link ? 'border-b-2' : ''
+                }`}
+              >
+                {item.text}
+              </div>
+            </Link>
+          </li>
+        )})}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
